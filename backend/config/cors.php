54c235e5cf79597d08c78c6510dd626de9efe64c
Eduarda <Adraude2694@gmail.com>
2026-07-10 10:18:41 +0100

    fix: permitir CORS do Expo Web nas portas 8081 e 8082

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,6 +17,8 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:8081',
         'http://127.0.0.1:8081',
+        'http://localhost:8082',
+        'http://127.0.0.1:8082',
     ])))),
 
     'allowed_origins_patterns' => [
